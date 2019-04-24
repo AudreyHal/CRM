e105ae5020a4ab51620d4a92809705d7efa7ca8b
damilola.o <damilola.o@ng.lopworks.com>
2019-04-24 16:53:17 +0100

    Fix wrong schema reference

--- lib/crm/sales/contact.ex
+++ lib/crm/sales/contact.ex
@@ -1,7 +1,7 @@
 defmodule Crm.Sales.Contact do
   use Ecto.Schema
   import Ecto.Changeset
-  alias MiniCrm.CRM.{Account}
+  alias MiniCrm.Sales.{Account}
 
   schema "contacts" do
     field :city, :string
