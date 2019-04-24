3bbb61c166631dcc499cc6841eae53b240e930a0
damilola.o <damilola.o@ng.lopworks.com>
2019-04-24 16:25:24 +0100

    Implement lead main features

--- test/crm_web/acceptance/lead_index_test.exs
+++ test/crm_web/acceptance/lead_index_test.exs
@@ -10,4 +10,15 @@ defmodule CrmWeb.Acceptance.LeadIndexTest do
     button = find_element(:css, ".btn-custom a") |> visible_text()
     assert button == "New Lead"
   end
+
+  test "presence of table when leads are available" do
+    navigate_to("/leads")
+  end
+
+  test "presence of error message when leads are absent" do
+    navigate_to("/leads")
+
+    text = find_element(:class, "empty-leads") |> visible_text()
+    assert text == "No leads created yet."
+  end
 end
