83d05412ae822a6912f149ee3070beb3cc20464a
damilola.o <damilola.o@ng.lopworks.com>
2019-04-24 16:51:23 +0100

    Create contact schema

---
+++ lib/crm/sales/contact.ex
@@ -0,0 +1,27 @@
+defmodule Crm.Sales.Contact do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias MiniCrm.CRM.{Account}
+
+  schema "contacts" do
+    field :city, :string
+    field :country, :string
+    field :department, :string
+    field :email, :string
+    field :first_name, :string
+    field :last_name, :string
+    field :phone, :string
+    field :street, :string
+    field :title, :string
+    belongs_to :accounts, Account, foreign_key: :account_id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(contact, attrs) do
+    contact
+    |> cast(attrs, [:city, :country, :department, :email, :first_name, :last_name, :phone, :street, :title])
+    |> validate_required([:city, :country, :department, :email, :first_name, :last_name, :phone, :street, :title])
+  end
+end
