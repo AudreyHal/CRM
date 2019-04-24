1e2a76ea2d0276217d35a50b9ee9e134c09c6381
damilola.o <damilola.o@ng.lopworks.com>
2019-04-24 11:23:38 +0100

    Change database config

--- config/dev.exs
+++ config/dev.exs
@@ -68,8 +68,8 @@ config :phoenix, :plug_init_mode, :runtime
 
 # Configure your database
 config :crm, Crm.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "appuser",
+  password: "Neue.P455",
   database: "crm_dev",
   hostname: "localhost",
   pool_size: 10
